6611880e7f3bc5f7ac9e6dab284cab04023f5518
Charlie Rusbridger <charlie@rusbridger.com>
2025-07-22 20:40:30 +0100

    Fix python39

--- setenv.sh
+++ setenv.sh
@@ -12,16 +12,16 @@ rm -f commands.sh
 python setenv.py $1 > commands.sh
 chmod +x commands.sh
 source commands.sh
-if [ "$(uname -o)" == "Cygwin" ]; then
+if [ "$(uname -o)" != "Cygwin" ]; then
     export PATH=/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:${DIR}/tools/mhmake/${release}:/mnt/c/nasm:$PATH:/mnt/c/gnuwin32/bin:/mnt/c/perl/perl/bin
     export MHMAKECONF=${DIR}
     export PYTHON3=/mnt/c/Python39/python.exe
     export WSLENV="${WSLENV}:MHMAKECONF/l:PYTHON3/l:IS64/l:CFLAGS/l"
 else
-    export PATH=${DIR}/tools/mhmake/${release}:$PATH
+    export PATH=${DIR}/tools/mhmake/${release}:$PATH:/mnt/c/Python39
     if [ -z "${MHMAKECONF}" ] ; then
         export MHMAKECONF=$(cygpath -w ${DIR})
-        export PYTHON3=c:\\Python39\\python.exe
+        export PYTHON3=python.exe
     fi
 fi
 rm -f commands.sh
