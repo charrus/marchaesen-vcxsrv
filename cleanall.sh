0a1b4670a0e527b0c81d35241332d7e2c36b393b
Charlie Rusbridger <charlie@rusbridger.com>
2025-07-27 22:44:25 +0100

    Sync to fix wsl builds

--- cleanall.sh
+++ cleanall.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 git clean -df
-find . -type d -name obj -exec rm -rf {} \; 2>/dev/null
-find . -type d -name objs -exec rm -rf {} \; 2>/dev/null
-find . -type d -name obj64 -exec rm -rf {} \; 2>/dev/null
-find . -type d -name obj32 -exec rm -rf {} \; 2>/dev/null
-find . -type d -name Release64 -exec rm -rf {} \; 2>/dev/null
-find . -type d -name Release -exec rm -rf {} \; 2>/dev/null
-find . -type d -name Debug64 -exec rm -rf {} \; 2>/dev/null
-find . -type d -name Debug -exec rm -rf {} \; 2>/dev/null
+git clean -df -x -e '**/obj/'
+git clean -df -x -e '**/objs/'
+git clean -df -x -e '**/obj64/'
+git clean -df -x -e '**/obj32'
+git clean -df -x -e '**/Release64/'
+git clean -df -x -e '**/Release/'
+git clean -df -x -e '**/Debug64/'
+git clean -df -x -e '**/Debug/'
