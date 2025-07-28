6af5251f878c995167eef218934ba7faacd51dc5
Charlie Rusbridger <charlie@rusbridger.com>
2025-07-28 17:20:08 +0100

    Fix for docker builds

--- xorg-server/xkeyboard-config/build.sh
+++ xorg-server/xkeyboard-config/build.sh
@@ -1,3 +1,7 @@
-meson setup -D=xorg-rules-copy=true --prefix=$(realpath ../xkbdata) builddir
+if [ -z "${CYGWIN}" ]; then
+  meson setup -D=xorg-rules-copy=true --prefix=$(realpath ../xkbdata) builddir
+else
+  meson setup -D=xorg-rules-copy=true --prefix=$(cygpath -w ../xkbdata) builddir
+fi
 cd builddir; meson compile; meson install
 
