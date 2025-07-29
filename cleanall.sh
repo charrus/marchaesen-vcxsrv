49020c7b32416e9fc53c5bcd80e6e6f124b45d63
Charlie Rusbridger <charlie@rusbridger.com>
2025-07-29 16:02:47 +0100

    Fix some typos and indenting

--- cleanall.sh
+++ cleanall.sh
@@ -4,7 +4,7 @@ git clean -df
 git clean -df -x -e '**/obj/'
 git clean -df -x -e '**/objs/'
 git clean -df -x -e '**/obj64/'
-git clean -df -x -e '**/obj32'
+git clean -df -x -e '**/obj32/'
 git clean -df -x -e '**/Release64/'
 git clean -df -x -e '**/Release/'
 git clean -df -x -e '**/Debug64/'
