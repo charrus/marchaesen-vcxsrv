3dfd894aeb92312adb84908f2a47cf51181022e4
Charlie Rusbridger <charlie@rusbridger.com>
2025-07-29 16:02:47 +0100

    Fix some typos and indenting

--- buildall.sh
+++ buildall.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
 if [ "${1}" == "1" ]; then
-  export IS64=1
+    export IS64=1
 elif [ "${1}" == "0" ]; then
-  export IS64=0
+    export IS64=0
 else
-  echo "Please pass 1 (64-bit compilation) or 0 (32-bit compilation) as first argument"
-  exit 1
+    echo "Please pass 1 (64-bit compilation) or 0 (32-bit compilation) as first argument"
+    exit 1
 fi
 if [ "${2}" == "" ] ; then
-  echo "Please pass number of parallel builds as second argument"
-  exit 1
+    echo "Please pass number of parallel builds as second argument"
+    exit 1
 fi
 if [ "${3}" == "" ] ; then
-  echo "Please pass build type as third argument.  D for DEBUG, R for RELEASE, A for ALL"
-  exit 1
+    echo "Please pass build type as third argument.  D for DEBUG, R for RELEASE, A for ALL"
+    exit 1
 fi
 if [ "${3}" == "A" ] ; then
-  export BUILDRELEASE=1
-  export BUILDDEBUG=1
+    export BUILDRELEASE=1
+    export BUILDDEBUG=1
 fi
 if [ "${3}" == "R" ] ; then
-  export BUILDRELEASE=1
-  export BUILDDEBUG=0
+    export BUILDRELEASE=1
+    export BUILDDEBUG=0
 fi
 if [ "${3}" == "D" ] ; then
-  export BUILDRELEASE=0
-  export BUILDDEBUG=1
+    export BUILDRELEASE=0
+    export BUILDDEBUG=1
 fi
+
 source ./setenv.sh ${IS64}
+
 export BUILDDEPS=1
 if [ "${4}" == "N" ] ; then
-  export BUILDDEPS=0
+    export BUILDDEPS=0
 fi
+
 function check-error {
     errorcode=$?
     if [[ $errorcode != 0 ]]; then
--- setenv.sh
+++ setenv.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 if [ "${1}" == "1" ]; then
-  export IS64=1
+    export IS64=1
 elif [ "${1}" == "0" ]; then
-  export IS64=0
+    export IS64=0
 else
-  echo "Please pass 1 (64-bit compilation) or 0 (32-bit compilation) as first argument"
-  exit 1
+    echo "Please pass 1 (64-bit compilation) or 0 (32-bit compilation) as first argument"
+    exit 1
 fi
 
 export MHMAKEPATH="tools/mhmake"
-
 if [ "${BUILDDEBUG}" == "1" ] ; then
     MHMAKEPATH="${MHMAKEPATH}/Debug"
 fi
 if [ "${BUILDRELEASE}" == "1" ] ; then
-    export MHMAKEPATH="tools/mhmake/Release"
+    MHMAKEPATH="${MHMAKEPATH}/Release"
 fi
 if [ "${IS64}" == "1" ]; then
-  export MHMAKEPATH="${MHMAKEPATH}64"
+    MHMAKEPATH="${MHMAKEPATH}64"
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -27,18 +26,18 @@ rm -f commands.sh
 python setenv.py $1 > commands.sh
 chmod +x commands.sh
 source commands.sh
+
 if [ -z "${CYGWIN}" ]; then
-    export PATH=/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:${DIR}/tools/mhmake/${MHMAKEPATH}:/mnt/c/nasm:$PATH:/mnt/c/gnuwin32/bin:/mnt/c/perl/perl/bin
+    # WSL build.
+    export PATH=/usr/local/bin:/usr/local/sbin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:${DIR}/${MHMAKEPATH}:/mnt/c/nasm:$PATH:/mnt/c/gnuwin32/bin:/mnt/c/perl/perl/bin
     export MHMAKECONF=${DIR}
-    export PYTHON3=/mnt/c/Python39/python.exe
     export WSLENV="${WSLENV}:MHMAKECONF/l:PYTHON3/l:IS64/l:CFLAGS/l"
 else
-    export PATH=${DIR}/tools/mhmake/${release}:$PATH:/mnt/c/Python39
-    if [ -z "${MHMAKECONF}" ] ; then
-        export MHMAKECONF=$(cygpath -w ${DIR})
-        export PYTHON3=python3
-    fi
+    # Docker cygwin build - $PATH is setup in Dockerfile so no need to set it here.
+    export PATH=${DIR}/${MHMAKEPATH}:$PATH
+    export MHMAKECONF=$(cygpath -w ${DIR})
 fi
+export PYTHON3=/mnt/c/Python39/python.exe
 rm -f commands.sh
 export CFLAGS="-FS"
 
