3369fa15e094bb2cf80b269a49343f21678de0cf
Charlie Rusbridger <charlie@rusbridger.com>
2025-07-30 11:02:06 +0100

    Add back in xkeyboard into main build to deprecate prepare.sh

--- xorg-server/xkeyboard-config/build.sh
+++ xorg-server/xkeyboard-config/build.sh
@@ -1,7 +1,13 @@
 if [ -z "${CYGWIN}" ]; then
-  meson setup -D=xorg-rules-copy=true --prefix=$(realpath ../xkbdata) builddir
+  meson setup --prefix=$(realpath ../xkbdata) builddir
 else
-  meson setup -D=xorg-rules-copy=true --prefix=$(cygpath -w ../xkbdata) builddir
+  meson setup --prefix=$(cygpath -w ../xkbdata) builddir
 fi
-cd builddir; meson compile; meson install
+pushd builddir
+meson compile
+meson install
+popd
 
+cp ../xkbdata/rules/base ../xkbdata/rules/xorg
+cp ../xkbdata/rules/base.lst ../xkbdata/rules/xorg.lst
+cp ../xkbdata/rules/base.xml ../xkbdata/rules/xorg.xml
