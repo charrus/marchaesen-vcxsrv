32f3f998c8c58f8493f55b4c959ef78c0b6c28cb
Charlie Rusbridger <charlie@rusbridger.com>
2025-07-31 16:34:27 +0100

    Update xkeyboard to 2.45

--- include/dix-config.h
+++ include/dix-config.h
@@ -377,7 +377,7 @@
 #undef XORG_RELEASE
 
 /* Current Xorg version */
-#define XORG_VERSION_CURRENT (((21) * 10000000) + ((1) * 100000) + ((16) * 1000) + 1)
+#define XORG_VERSION_CURRENT (((21) * 10000000) + ((1) * 100000) + ((16) * 1000) + 2)
 
 /* Xorg release date */
 #define XORG_DATE "7 April 2020"
