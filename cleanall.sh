82934e23bb4ee1420f84315ec1283dcc54c12524
Charlie Rusbridger <charlie@rusbridger.com>
2025-09-17 13:00:04 +0100

    Add cleanup to submodules and add instructions to how to build with submodules

--- cleanall.sh
+++ cleanall.sh
@@ -9,3 +9,7 @@ git clean -df -x -e '**/Release64/'
 git clean -df -x -e '**/Release/'
 git clean -df -x -e '**/Debug64/'
 git clean -df -x -e '**/Debug/'
+
+git submodule foreach 'git clean -df'
+git submodule foreach 'git restore .'
+git submodule foreach 'rm -rf * .[a-z]*'
